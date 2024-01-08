40bc6a0777a7f7871d541a7372ea745046a63315
dev.abusalem <dev.abusalem@gmail.com>
2024-01-08 23:22:58 +0600

    update again

--- src/app/admin/url-manager/url-manager.js
+++ src/app/admin/url-manager/url-manager.js
@@ -1,3 +1,3 @@
 //export const BASE_URL = process.env.LIVE_BACKEND_URL;
-
-export const BASE_URL = "https://server-ten-dun-58.vercel.app/";
+cp;
+export const BASE_URL = "https://leaf-backend-mu.vercel.app";
